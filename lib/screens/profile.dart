759b3d33ed14d9c5d4c24dd447d3796dfa56b41b
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2023-10-27 01:04:52 +0800

    added iran politics

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -211,6 +211,144 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ),
                     ],
                   ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Elly's Post",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Gellix',
+                          fontSize: 17,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                          color: Color(0xFF5474FD),
+                          fontFamily: 'Gellix',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.white,
+                      ),
+                      child: Container(
+                        height: 90,
+                        width: 90,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/iran.png'))),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "News: Politics",
+                          style: TextStyle(
+                              color: Color(0xFF9397A0),
+                              fontFamily: 'Gellix',
+                              fontSize: 10,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Iran's Raging Protests",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Gellix',
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "Fifth Iranian paramilitary me...",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Gellix',
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 17,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/images/calendar_icon.png'))),
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Text(
+                                  "16th May",
+                                  style: TextStyle(
+                                      color: Color(0xFF9397A0),
+                                      fontFamily: 'Gellix',
+                                      fontSize: 11,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 56,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 17,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/images/time_icon.png'))),
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Text(
+                                  "09:32pm",
+                                  style: TextStyle(
+                                      color: Color(0xFF9397A0),
+                                      fontFamily: 'Gellix',
+                                      fontSize: 11,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
                 )
               ],
             ),
